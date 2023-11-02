ad1888b61bc70adb8d437d92119436e83b10cd7c
harbans-kaur <harbanskaur1602@gmail.com>
2023-11-02 16:21:37 +0530

    files added

---
+++ resources/views/mailVerification.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>{{$data['title']}}</title>
+</head>
+<body>
+    <p>This side Himanshu Sharma</p>
+    <br>
+    <p>{{ $data['body'] }}</p>
+    <br>
+    <p>Thank You!</p>
+
+</body>
+</html>
